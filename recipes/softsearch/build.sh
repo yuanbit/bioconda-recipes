61937c454cc2711597f006f7d6c616c400b72e9c
Russell Littman <rlittman@login4.(none)>
2018-05-20 21:12:34 -0700

    added softsearch

---
+++ recipes/softsearch/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://s3-us-west-2.amazonaws.com/mayo-bic-tools/softsearch/Softsearch2.4.tar.gz
+tar -xvzf Softsearch2.4.tar.gz
+mv 2.4 Softsearch2.4
+cd Softsearch2.4
+perl ./install.pl --prefix=$PWD
+export PERL5LIB=$PERL5LIB:$PWD:$PWD/lib
