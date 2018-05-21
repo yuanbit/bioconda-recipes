3fe5b3c51be96dfce77f12eb90a751113f17075f
Russell Littman <rlittman@login4.(none)>
2018-05-20 22:00:09 -0700

    added hash to softsearch

--- recipes/softsearch/build.sh
+++ recipes/softsearch/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget https://s3-us-west-2.amazonaws.com/mayo-bic-tools/softsearch/Softsearch2.4.tar.gz
-wget -0- https://s3-us-west-2.amazonaws.com/mayo-bic-tools/softsearch/Softsearch2.4.tar.gz | shasum -a 256
 tar -xvzf Softsearch2.4.tar.gz
 mv 2.4 Softsearch2.4
 cd Softsearch2.4
